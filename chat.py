aa444521e0212293d77f9c2bb49b4303aab27a3b
Omkar Kakkad <72384721+omkarkakkad@users.noreply.github.com>
2022-03-10 14:22:22 +0530

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,14 @@
+print("Welcome to chat App.")
+
+name = input("Enter your name here: ")
+print("Hello", name)
+
+greeting = input("How are you?")
+print("great!!!")
+
+std = input("In which class are you studying?")
+print("Nice!!")
+print("Happy to meet you.")
+
+end = input("Type something: ")
+print("Over")
